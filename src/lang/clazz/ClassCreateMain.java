001a1274353baa0210994c42b68cd12eb55f07f6
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2025-08-28 15:32:55 +0900

    feat) 래퍼클래스 6

---
+++ src/lang/clazz/ClassCreateMain.java
@@ -0,0 +1,15 @@
+package lang.clazz;
+
+public class ClassCreateMain {
+    public static void main(String[] args) throws Exception {
+        Class helloClass = Hello.class;
+//        Class helloClass2 = Class.forName("lang.clazz.Hello");//패키지명
+
+        Hello hello = (Hello) helloClass.getDeclaredConstructor().newInstance();
+        String result = hello.hello();
+        System.out.println("hello = " + hello);
+        System.out.println("result = " + result);
+    }
+
+
+}
